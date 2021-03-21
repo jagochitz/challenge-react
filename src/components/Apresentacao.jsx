7d7a0b36ddea60950bbc98764fbdd2efa042e1cc
Rafael Jagochitz <jagochitz@gmail.com>
2021-03-20 23:50:00 -0300

    realizado ajustes no header e apresentação.

--- src/components/Apresentacao.jsx
+++ src/components/Apresentacao.jsx
@@ -4,16 +4,22 @@ import CellsMini from "../images/Cells-mini.png";
 
 export default (props) => (
   <section className="app-apresentacao">
-    <h3>Food app</h3>
-    <h1>Why stay hungry when</h1>
-    <h1 className="h1">you can order form Bella Onojie</h1>
-    <p>Download the bella onoje's food app now on</p>
-    <div className="buttons-apresentacao">
-      <div className="buttons-apresentacao-item">
-        <button>Playstore</button>
+    <div className="apres-content">
+      <h3>Food app</h3>
+      <div className="apres-titulo">
+        <h1>Why stay hungry when</h1>
+        <h1>you can order form Bella Onojie</h1>
       </div>
-      <div className="buttons-apresentacao-item">
-        <button>App store</button>
+      <div className="apres-p">
+        <p>Download the bella onoje's food app now on</p>
+      </div>
+      <div className="buttons-apresentacao">
+        <div className="buttons-apresentacao-item">
+          <button>Playstore</button>
+        </div>
+        <div className="buttons-apresentacao-item">
+          <button>App store</button>
+        </div>
       </div>
     </div>
     <div className="cells-mini">
